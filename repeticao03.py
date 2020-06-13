d3c1685db531f881f9d635f9f853f9edf16f90cd
Thayse Cristina de Sousa <61096@PROWAY.TREINA>
2020-06-13 11:45:39 -0300

    Aula 02 - FOR

---
+++ repeticao03.py
@@ -0,0 +1,10 @@
+#percorrendo listas com for
+
+#nós só temos as posições com o range
+numeros = range(5)
+
+lista_idades = [10,15,21,48]
+
+#percorrendo os valores da lista
+for i in lista_idades:
+    print(i)
