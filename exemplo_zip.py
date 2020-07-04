ff6d42fdeb2f8c66fc66ea1ae87c47b147ab5bd2
Thayse Cristina de Sousa <61096@PROWAY.TREINA>
2020-07-04 12:52:44 -0300

    Aula 05

--- exemplo_zip.py
+++ exemplo_zip.py
@@ -1,4 +1,12 @@
-arquivo = open('cadastro.txt', 'r')
+# Abra a lista cadastro3.txt
+# Crie uma List Comprehension e zip que trate e converta cada pessoa em um
+#  dicionário
+# Salve os dicionários em uma lista
+# Encontre todos que possuam idade de 18 a 25 anos.
+# Salve o resultado em um arquivo .txt
+
+arquivo = open('cadastro3.txt', 'r')
+arquivo2 = open('cadastro4.txt','w')
 
 lista = []
 for pessoa in arquivo:
@@ -10,4 +18,8 @@ arquivo.close()
 chaves = ["id","nome","idade","sexo","email","telefone"]
 lista_cadastro = [dict(zip(chaves,pessoa)) for pessoa in lista]
 
-print(lista_cadastro)
+pessoa2 = [x for x in lista_cadastro if int(x['idade']) >=18 and int(x['idade']) <=25]
+print(pessoa2)
+
+arquivo2.write(pessoa2)
+arquivo2.close()
---
+++ gerador.py
@@ -0,0 +1,2 @@
+# 1) Crie um gerador que em cada interação retorne uma letra do alfabeto
+# 2) Crie um interado que receba uma lista qualquer e retorne cada elemento desta lista.
