731c9a5628e840e76d06dde51ed4b3966bd16f09
Thayse Cristina de Sousa <61096@PROWAY.TREINA>
2020-07-11 11:45:09 -0300

    Aula 6

--- programacao_funcional.py
+++ programacao_funcional.py
@@ -34,3 +34,31 @@ montante = (200*(1+(10/100)))
 espo = lambda t,valor = 200, i= 10:round(valor*(1+(i/100))**t)
 lista_exemplo2 = list(map(espo,tempo))
 print(lista_exemplo2)
+
+# Exercicio_filtro
+lista_numeros = [i for i in range(31)]
+filtro = lambda x : True if (x%3)==0 else False
+resultado = list(filter(filtro,lista_numeros))
+print(resultado)
+
+# sortear valores aleatórios
+from random import randint
+print(randint(0,100))
+# ou
+lista_random = [randint(100,1000) for i in range(28)] #lista com valores aleatórios
+print(lista_random)
+
+#reduce
+from functools import reduce
+
+lista_reduce = [i for i in range(10)]
+funcao = lambda x, y: x+y
+print(reduce(funcao,lista_reduce))
+
+lista_reduce = [i for i in range(1,10)]
+funcao = lambda x, y: x*y
+print(reduce(funcao,lista_reduce))
+
+lista_reduce = [1,1,2,2,3,3,4,5,5]
+funcao = lambda x, y: x^y
+print(reduce(funcao,lista_reduce))
